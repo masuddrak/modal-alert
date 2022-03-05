bb49555bc416ab0ce740a93918e97df85c9a7d64
masudrana <masudranaacb@gmail.com>
2022-03-05 19:07:21 +0600

    confirm respons

--- modal.js
+++ modal.js
@@ -1,3 +1,13 @@
 const alert1=()=>{
     alert('hello')
 }
+// payBill
+const payBill=()=>{
+   const respons= confirm('please pay bill 200 taka')
+   if(respons===true){
+       console.log('thakns you')
+   }
+   else{
+       console.log('you are on select')
+   }
+}
